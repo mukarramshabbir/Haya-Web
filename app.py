4c6e3c87c533512e8704216b002c43a7181ed32b
Mukarram Ali <mukarramali623@gmail.com>
2024-08-19 04:37:44 +0500

    first commit

--- app.py
+++ app.py
@@ -51,3 +51,6 @@ def submit_restaurant():
             print(f"Meal Image saved to: {meal_image_path}")
     
     return redirect(url_for('index'))
+
+if __name__ == "__main__":
+    app.run(debug=True)
